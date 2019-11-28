4538c267d9356a055e79414b062a2d38012d1859
A Q Khan <aqk.neo@gmail.com>
2019-11-28 16:11:45 +0500

    basic setup

---
+++ pages/index.js
@@ -0,0 +1,42 @@
+import React from 'react'
+import Head from 'next/head'
+import '../Styles.sass'
+
+const Home = () => (
+  <div>
+    <Head>
+      <title>Home</title>
+      <link rel="icon" href="/favicon.ico" />
+    </Head>
+    <div>
+      <h1 className="title">Bulma</h1>
+      <p className="subtitle">
+        Modern CSS framework based on{' '}
+        <a href="https://developer.mozilla.org/en-US/docs/Web/CSS/CSS_Flexible_Box_Layout/Basic_Concepts_of_Flexbox">Flexbox</a>
+      </p>
+
+      <div className="field">
+        <div className="control">
+          <input className="input" type="text" placeholder="Input" />
+        </div>
+      </div>
+
+      <div className="field">
+        <p className="control">
+          <span className="select">
+            <select>
+              <option>Select dropdown</option>
+            </select>
+          </span>
+        </p>
+      </div>
+
+      <div className="buttons">
+        <a className="button is-primary">Primary</a>
+        <a className="button is-link">Link</a>
+      </div>
+    </div>
+  </div>
+)
+
+export default Home
